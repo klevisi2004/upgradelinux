758ea0522275ffaae1565bad2b37484d51d21615
FR13NDS <62477193+klevisi2004@users.noreply.github.com>
2021-02-18 17:07:41 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Upgrade Linux
+### upgrade your linux device 
+
+![capture](https://user-images.githubusercontent.com/62477193/108432924-c1b47280-720a-11eb-95a3-12c1fdbb847b.png)
+
+
+### Follow me
+##### github.com/klevisi2004
+##### instagram.com/klevis_r1
+##### linkedin.com/in/klevis-ruci
+###### klevis ruci
---
+++ upgradelinux.sh
@@ -0,0 +1,44 @@
+# Coded by: klevisi2004
+# github.com/klevisi2004
+# instagram.com/klevis_r1
+printf "\e[2;92m     coded by: klevisi2004\e[0m\n"
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m  Starting System upgrade \e[0m\e[1;31m * \e[0m\n"
+printf "\e[1;31m *                           *\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 3
+printf "\e[1;31m *    Press\e[0m\e[1;32m Y\e[0m\e[1;31m wen needed     *\e[0m\n"
+printf "\e[1;31m *                           *\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 3
+printf "\e[1;52m               -              \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m      Downloading Git   \e[0m\e[1;31m   * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 2
+sudo apt install git 
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m      Downloading php \e[0m\e[1;31m     * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 1
+sudo apt install php
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m      Downloading Wine \e[0m\e[1;31m    * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 1
+sudo apt install wine
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m    Downloading upgrades \e[0m\e[1;31m  * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 1
+sudo apt upgrade
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m       Congratulate!\e[0m\e[1;31m       * \e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m    System it's upgraded   \e[0m\e[1;31m* \e[0m\n"
+printf "\e[1;31m *****************************\e[0m\n"
+sleep 1
