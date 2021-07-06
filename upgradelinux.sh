96b1b49b76a315c711f299d1341568c2fc53386e
FR13NDS <62477193+klevisi2004@users.noreply.github.com>
2021-07-06 09:00:13 -0500

    Update upgradelinux.sh

--- upgradelinux.sh
+++ upgradelinux.sh
@@ -12,6 +12,19 @@ printf "\e[1;31m *    Press\e[0m\e[1;32m Y\e[0m\e[1;31m wen needed     *\e[0m\n"
 printf "\e[1;31m *                           *\n"
 printf "\e[1;31m ***************************** \e[0m\n"
 sleep 3
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m    Downloading updates \e[0m\e[1;31m   * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 1
+sudo apt update
+printf "\n"
+printf "\e[1;31m *****************************\e[0m\n"
+printf "\e[1;31m *\e[0m\e[1;32m    Downloading upgrades \e[0m\e[1;31m  * \e[0m\n"
+printf "\e[1;31m ***************************** \e[0m\n"
+sleep 1
+sudo apt upgrade
+printf "\n"
 printf "\e[1;52m               -              \e[0m\n"
 printf "\e[1;31m ***************************** \e[0m\n"
 printf "\e[1;31m *\e[0m\e[1;32m      Downloading Git   \e[0m\e[1;31m   * \e[0m\n"
@@ -30,18 +43,6 @@ printf "\e[1;31m *\e[0m\e[1;32m      Downloading Wine \e[0m\e[1;31m    * \e[0m\n
 printf "\e[1;31m ***************************** \e[0m\n"
 sleep 1
 sudo apt install wine
-printf "\n"
-printf "\e[1;31m *****************************\e[0m\n"
-printf "\e[1;31m *\e[0m\e[1;32m    Downloading upgrades \e[0m\e[1;31m  * \e[0m\n"
-printf "\e[1;31m ***************************** \e[0m\n"
-sleep 1
-sudo apt upgrade
-printf "\n"
-printf "\e[1;31m *****************************\e[0m\n"
-printf "\e[1;31m *\e[0m\e[1;32m    Downloading updates \e[0m\e[1;31m   * \e[0m\n"
-printf "\e[1;31m ***************************** \e[0m\n"
-sleep 1
-sudo apt update
 printf "\n"
 printf "\e[1;31m *****************************\e[0m\n"
 printf "\e[1;31m *\e[0m\e[1;32m       Congratulate!\e[0m\e[1;31m       * \e[0m\n"
